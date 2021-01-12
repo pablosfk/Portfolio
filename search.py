1a7c100f4c1997e4d0e241827df61ec2db43a51e
pablosfk <pablosfk@gmail.com>
2021-01-12 16:36:33 -0300

    V 1.0

--- search.py
+++ search.py
@@ -1,6 +1,6 @@
 def linSearch(list, value):
-''' This function will search a value in a list
-using linear search method.'''
+  ''' This function will search a value in a list using linear search method.'''
+  #If receive True, the second number indicates the position of the found value into the list
 
   found = False
   pos = 0
@@ -9,5 +9,11 @@ using linear search method.'''
       found = True
       pos = i
       break
-      
-return found, pos
+
+  return found, pos
+
+a = [1, 15, 2, 75, 5, 'ab', 23, 17, 'a', 9, 152, 247, 3, 31, 29]
+print(linSearch(a,3))
+print(linSearch(a,32))
+print(linSearch(a,'a'))
+print(linSearch(a,'b'))
