12b6972dd7d78c5d91c443d72ff52f616c0a6d6f
pablosfk <pablosfk@gmail.com>
2021-01-16 03:51:29 -0300

    V 3.0 insertion, selection, heap

--- sort.py
+++ sort.py
@@ -44,3 +44,38 @@ print(selection(lista))
 print(selection(lista1))
 '''
 
+
+def heap(list):
+    #Los hijos son list(i*2) y list(i*2+1) siendo i los padres, y padres son todos aquellos donde se cumple esa
+    #ecuación sin sobrepasar el largo de la lista: list(i*2) < len(list)-1 y list(i*2+1) < len(list)-1
+    list_out = []
+    while 1:
+        length = len(list)
+        rang = range(1, length)
+        ready = True
+        for i in rang:
+            if 2*i-1 < length and list[2*i-1] > list[i-1]: #if the length is enough and son bigger than father
+                aux = list[i-1]
+                list[i-1] = list[2*i-1]
+                list[2*i-1] = aux
+                ready = False
+            elif 2*i < length and list[2*i] > list[i-1]:
+                aux = list[i-1]
+                list[i-1] = list[2*i]
+                list[2*i] = aux
+                ready = False
+        if ready:
+            list_out.append(list[0])
+            list.pop(0)
+        if not list:
+            list_out.reverse()
+            break
+    return list_out
+
+
+'''
+lista = [39, 83, 37, 18, -12, 73, 52, 1, 26, 76, 67, 20, 10, -3, 62, 28, 82, 91, 81, -58, 50, 72, 89, 29, 1]
+lista1 = ['boca', 'carreta', 'jalea', 'altura', 'zorra', 'corral', 'batata']
+print(heap(lista))
+print(heap(lista1))
+'''
